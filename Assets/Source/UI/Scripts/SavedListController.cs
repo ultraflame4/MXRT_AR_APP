b3c4d52f3b6f051e6bc016585f2599c8ad4a899f
ultr42 <ultr42@skiff.com>
2024-06-06 12:24:10 +0800

    more or less finished saved menu

---
+++ Assets/Source/UI/Scripts/SavedListController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class SavedListController : MonoBehaviour
+{
+    public GameObject listItemPrefab;
+
+    public void Populate()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+
+        foreach (var item in AppController.Instance.interestPointsDatabase.interestPoints.Where(x => x.saved))
+        {
+            var listItem = Instantiate(listItemPrefab, transform).GetComponent<SearchListItem>();
+            listItem.data = item;
+        }
+    }
+
+    private void Start()
+    {
+        AppController.Instance.tabManager.TabChanged += OnTabChanged;
+    }
+
+    private void OnTabChanged(TabManager.TabData data, bool isSameTab)
+    {
+
+        Populate();
+    }
+
+}
